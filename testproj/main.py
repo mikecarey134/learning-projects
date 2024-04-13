07033881213e9453e9b31aca76cd8a3e80a074d4
mikecarey134 <mikecarey134@gmail.com>
2024-04-12 20:39:10 -0400

    Added password program and interface updates

--- testproj/main.py
+++ testproj/main.py
@@ -8,6 +8,29 @@
 ######################
 
 import random
+import os
+
+def password_gen(passwd_size):
+    
+    password = ""
+    special_chars ="!@#$%^&*()-_=+[]{}~?<>"
+    lower_case = "qwertyuiopasdfghjklzxcvbnm"
+    upper_case = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    nums ="1234567890"
+    count =0
+
+    while count <= passwd_size:
+
+        password += special_chars [random.randint(0, len(special_chars) -1    )       ]
+        password += lower_case    [random.randint(0, len(lower_case)    -1    )       ]
+        password += upper_case    [random.randint(0, len(upper_case)    -1    )       ]
+        password += nums          [random.randint(0, len(nums)          -1    )       ]
+        count = count +1
+
+    password = password[:passwd_size]
+    
+    print("Generated Password: "+ password)
+
 
 #definition of one of our mini games
 def hi_low_game():
@@ -25,12 +48,13 @@ def hi_low_game():
         #get user input and set it as the variable guess
         guess = input("Guess the number between 1-10) :")
 
+        #filter out bad input
         for char in invalid_chars:
             if guess.__contains__(char):
                 invalid_char_detetced = True
 
         if guess == '' or invalid_char_detetced:
-            print("Invalid iynput please try again")
+            print("Invalid input please try again")
         else:
             guess = int(guess)
             #check conditions high , low , and , correct guess
@@ -48,10 +72,19 @@ def interface():
 
     choice = ""
     while choice != "q":
-        choice = input("Would you like to play a game? y/n :")
+        choice = input("Would you like run a program? y/n/q :")
 
         if choice == "y":
-            hi_low_game()
+            program = input("Choose a program 1 High Low Game 2 Password Generator: ")
+            match program:
+                case "1":
+                    hi_low_game()
+                case "2":
+                    #password generator
+                    length = input("Password Length: ")
+                    password_gen(int(length))
+                case _:
+                    print("invalid option")
 
 #main function for the application
 def main():
