7b37f1d9a8708ef4fb119df70730e65438cb58d3
Mike Carey <mikecarey134@gmail.com>
2024-04-24 23:13:01 -0400

    updated steamcmd on valheim image

--- docker/valheim/steamcmd/start_vh_server.sh
+++ docker/valheim/steamcmd/start_vh_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
@@ -9,6 +8,7 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name "Reinchord" -port 2456 -world "Dedicated" -password "Michael134" -modifier resources more
+./valheim_server.x86_64 -name "Orchaliaum" -port 2456 -nographics -batchmode -world "Dedicated" -password "Michael134" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
+
